c6b5e82b1ce0f7f96f977c446c32366f80f93e23
Ekaterina Dronova <katedronova96@gmail.com>
2023-08-08 00:40:27 +0300

    feat: add library and button function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './button/button.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { ButtonComponent } from './button/button.component';
     ButtonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-button',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class ButtonComponent {
 
+  faCoffee = faCoffee;
+  fas = fas;
+
+  icons = ["house", "envelope", "star", "image", "heart", "file", "bell", "bolt", "ghost", "car", "book", "fire", "sun", "bug", "fish"];
+  i: number = 0;
+
+  showNewIcon(): void {
+    setTimeout(() => {
+      this.i = Math.floor(Math.random() * 15);
+    }, 3000)
+  }
 }
